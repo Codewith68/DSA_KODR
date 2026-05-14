98d9fac99d6a130069f87a657a9c0e2b2dc420ef
Subrat Palei <subratpaleibabul@gmail.com>
2026-05-14 16:31:14 +0530

    Remove redundant comment in 192.

--- 4th_week/192.cpp
+++ 4th_week/192.cpp
@@ -14,8 +14,7 @@
 
 // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
 // Output: true
-// Example 2:
-
+// Example 2
 
 // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 // Output: false
