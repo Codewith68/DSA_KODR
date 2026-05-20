4f380c66a7c939d36f7366a9dd6c7ca47be9e9da
Subrat Palei <subratpaleibabul@gmail.com>
2026-05-20 21:39:35 +0530

    Add comments and example for candyType function
    Added comments and example for candyType function.

--- 4th_week/161.cpp
+++ 4th_week/161.cpp
@@ -7,7 +7,6 @@
 // Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
 
  
-
 // Example 1:
 
 // Input: candyType = [1,1,2,2,3,3]
