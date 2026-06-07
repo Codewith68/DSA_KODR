4b7c6f6291d377d6deabbac623e0017add1aa1b2
Subrat Palei <subratpaleibabul@gmail.com>
2026-06-07 10:05:41 +0530

    Update comments for profit calculation examples

--- 3rd_week/149.cpp
+++ 3rd_week/149.cpp
@@ -13,7 +13,6 @@
 // Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
 // Total profit is 4 + 3 = 7.
 // Example 2:
-
 // Input: prices = [1,2,3,4,5]
 // Output: 4
 // Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
