b9ea13987c2863873f359d4eb434eb018c5e5542
Subrat Palei <subratpaleibabul@gmail.com>
2026-06-11 12:23:50 +0530

    Fix formatting issues in 186.cpp

--- 4th_week/186.cpp
+++ 4th_week/186.cpp
@@ -2,7 +2,6 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
-
 int coutnFreq(vector<int>& nums, int target){
     int lower=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
     int upper=upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1;
