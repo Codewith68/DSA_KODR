ea4accfdd9472a410ef9dc98772767ec4ded6135
Subrat Palei <subratpaleibabul@gmail.com>
2026-07-10 22:33:33 +0530

    Remove unnecessary comments in 123.cpp

--- 3rd_week/123.cpp
+++ 3rd_week/123.cpp
@@ -7,7 +7,6 @@
 // Return words after performing all operations. It can be shown that selecting the indices for each operation in any arbitrary order will lead to the same result.
 
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase using all the original letters exactly once. For example, "dacb" is an anagram of "abdc".
-
 // Example 1:
 
 // Input: words = ["abba","baba","bbaa","cd","cd"]
