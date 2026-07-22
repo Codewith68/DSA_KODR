65d07942c5f5705584d7c7f06cfb0aafea7c164d
Subrat Palei <subratpaleibabul@gmail.com>
2026-07-22 21:29:57 +0530

    Add function purpose comment and example
    Added a comment explaining the function's purpose and provided an example of its usage.

--- 3rd_week/114.cpp
+++ 3rd_week/114.cpp
@@ -3,7 +3,6 @@
 // Given a string s and a character letter, return the percentage of characters in s that equal letter rounded down to the nearest whole percent.
 
  
-
 // Example 1:
 
 // Input: s = "foobar", letter = "o"
