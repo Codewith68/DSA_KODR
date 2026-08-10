fd989e8a0054d5d91ffaaa524992ffd983f09305
Subrat Palei <subratpaleibabul@gmail.com>
2026-08-10 08:53:33 +0530

    Update comments for clarity in 132.cpp

--- 3rd_week/132.cpp
+++ 3rd_week/132.cpp
@@ -10,7 +10,6 @@
 // Output: [1,0,0,2,3,0,0,4]
 // Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
 // Example 2:
-
 // Input: arr = [1,2,3]
 // Output: [1,2,3]
 // Explanation: After calling your function, the input array is modified to: [1,2,3]
